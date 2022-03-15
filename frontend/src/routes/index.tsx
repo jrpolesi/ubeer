c82cd6aaef1a47bac1da5e7db57a9abb0f30f0c1
Monalisa Menezes <psimonafmenezes@gmail.com>
2022-03-15 20:36:15 -0300

    feat: Rotas comentadas, aguardando páginas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,15 @@
 import {  Routes, Route } from "react-router-dom"
-import Home from "../pages/Home"
+//import Home from "../pages/Home"
 
 const RoutesPages = () => {
     return(
         <Routes>
-            <Route path="/" element={<Home/>}/ >
+            {/* <Route path="/" element={<Home/>}/ >
             <Route path="signup" element={}/>            
             <Route path="login" element={}/>
             <Route path="dashboard" element={}/>
             <Route path="wallet" element={}/>
-            <Route path="history" element={}/>
+            <Route path="history" element={}/> */}
         </Routes>
     )
 } 
