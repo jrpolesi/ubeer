ff775c6f439cf8180921341ea3fdb329eb446daa
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-03-16 01:34:55 -0300

    feat: Adiciona persistencia do token no localStorage

--- frontend/src/providers/user/index.tsx
+++ frontend/src/providers/user/index.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, ReactNode, useState } from "react";
+import React, { createContext, ReactNode, useEffect, useState } from "react";
+import api from "../../services/api";
 import { UserContextInterface, UserData } from "../../types/userContext";
 
 export const UserContext = createContext<UserContextInterface>(
@@ -6,20 +7,33 @@ export const UserContext = createContext<UserContextInterface>(
 );
 
 export function UserProvider({ children }: { children: ReactNode }) {
-  const [token, setToken] = useState<string | false>(false);
+  const [token, setToken] = useState<string | null>(
+    JSON.parse(localStorage.getItem("@uBeer:token") || "null")
+  );
 
-  const [user, setUser] = useState<UserData>({} as UserData);
+  const [user, setUser] = useState<UserData | null>({} as UserData);
 
   function updateToken(newToken: string) {
     setToken(newToken);
+
+    localStorage.setItem("@uBeer:token", JSON.stringify(newToken));
   }
 
   function updateUser(userUpdated: UserData) {
     setUser(userUpdated);
   }
 
+  function logOut() {
+    setToken(null);
+    setUser(null);
+
+    localStorage.removeItem("@uBeer:token");
+  }
+
   return (
-    <UserContext.Provider value={{ updateUser, user, updateToken, token }}>
+    <UserContext.Provider
+      value={{ updateUser, user, updateToken, token, logOut }}
+    >
       {children}
     </UserContext.Provider>
   );
