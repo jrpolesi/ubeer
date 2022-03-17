c60b8c58185698c12a98c58da32a9dead187b05b
carlosv117 <limacarlosvitor@gmail.com>
2022-03-17 18:25:52 -0300

    feat: Styled para a página da Carteira

--- frontend/src/pages/Carteira/index.tsx
+++ frontend/src/pages/Carteira/index.tsx
@@ -1,47 +1,67 @@
 import React from "react";
-import { Box, Button, Image } from "grommet";
-import { Menu, Qr } from "grommet-icons";
-import { Header, Main } from "./styles";
-import LogoSalado from "../../assets/img/dinheiro.png";
 import { useNavigate } from "react-router-dom";
+// import { useContext } from "react";
+// import {UserContext} from "../../providers/user/index";
+// import api from "../../services/api";
+import { Footer, Header, Main, Section } from "./styles";
+import { Menu } from "grommet-icons";
+import LogoSalado from "../../assets/img/dinheiro.png";
+import QrCode from "../../assets/img/QRCode1.png";
+import Button from "../../components/Button";
 
 const Carteira = () => {
+  let wallet = 100;
+
+  // const {updateUser, user} = useContext(UserContext);
+  // const {budget ,id} = user;
+  // console.log(updateUser);
+  // console.log(user);
+  // console.log(budget);
+
+  const handleClick = (value: number) =>{
+    console.log(wallet += value);
+
+    // const allValue = budget + value;
+    // console.log(allValue);
+
+    // api.put(`/users/${id}/budget?value=${allValue}`);
+
+  };
+
+  
+
   const navigate = useNavigate();
 
   return(
     <>
       <Header>
-        <Box background="white" style={{borderRadius: 90, boxShadow: "1px 1px 8px #ccc", width: 36, height: 36}} align="center" justify="center">
-          <Button
-            icon={<Menu color="accent-4" />}
-            hoverIndicator
-            onClick={() => console.log("Menu")}
-          />
-        </Box>
+        <figure>
+          <Menu color="accent-4"/>
+        </figure>
         <h1>Saldo</h1>
       </Header>
       <Main>
-        <section>
+        <Section>
           <figure>
             <img src={LogoSalado} alt="Logo do salado" />
           </figure>
           <p>Adicione saldo na sua carteira</p>
           <div>
-            <input name="R$ 50,00" type="button" value="50" />
-            <input name="R$ 100,00" type="button" value="100" />
+            <button className="Adicionar" onClick={() => handleClick(50)}>R$ 50,00</button>
+            <button className="Adicionar" onClick={() => handleClick(100)}>R$ 100,00</button>
           </div>
           <figure className="QrCode">
-            <Qr/>
+            <img src={QrCode} alt="QR Code" />
           </figure>
-          <Button primary label="Adicionar saldo" onClick={() => console.log("Atualizar o saldo")} style={{borderRadius: 30}}/>
-        </section>
-        <footer>
+          <Button variant="primary" onClick={() => console.log("Atualizar o saldo")}>Adicionar saldo</Button>
+        </Section>
+        <Footer>
           <div>
             <p>O seu saldo atual é:</p>
-            <span>R$ 150,00</span>
+            <input value={`R$ ${wallet} `} />
           </div>
-          <Button primary label="Ok" onClick={() => navigate("/Dashboard")} />
-        </footer>
+          <Button variant="primary" onClick={() => navigate("/Dashboard")}>Ok</Button>
+        </Footer>
       </Main>
     </>
   );
--- frontend/src/pages/Carteira/styles.ts
+++ frontend/src/pages/Carteira/styles.ts
@@ -6,11 +6,18 @@ export const Header = styled.header`
     align-items: center;
     justify-content: space-between;
     background-color: #F7F8F9;
-    padding: 0px 25px;
+    padding: 0px 20px;
     height: 66px;
 
-    div{
-        
+    figure{
+        display: flex;
+        border-radius: 90px; 
+        box-shadow: 1px 1px 8px #ccc;
+        width: 36px;
+        height: 36px;
+        align-items: center;
+        justify-content: center;
+        opacity: 100;
     }
 `;
 
@@ -20,20 +27,106 @@ export const Main = styled.main`
     justify-content: space-between;
     flex-direction: column;
     background-color: #F7F8F9;
-    padding: 0px 25px;
-    height: 500px;
+    padding: 0px 20px;
 
-    section{
-        width: 100%;
-        height: 100%;
-        background-image: url(${Fundo});
-        background-size: contain;
+    button{
+        width: 85%;
+        border-radius: 15px;
+        cursor: pointer;
     }
 
-    h1{
+    input{
+        width: 45%;
+        height: 40px;
+        background-color: white;
+        border: none;
+        border-radius: 15px;
+        box-shadow: 1px 1px 8px #ccc;
+    }
+`;
+
+export const Section = styled.section`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background-color: #FFF;
+        box-shadow: 1px 1px 8px #ccc;
+        padding: 20px 0px;
+
+        figure{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 80px;
+        height: 80px;
+        background-color: #F7F8F9;
+        border-radius: 90px;
+    }
+    
+    p{
+        text-align: center;
+        width: 60%;
+        margin: 10px 0px;
+    }
+
+    div{
+        display: flex;
+        justify-content: space-between;
+        width: 85%;
     }
 
     .QrCode{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 85%;
+        height: 220px;
         background-color: #FBD50E;
+        border-radius: 15px;
+        margin: 20px 0px;
     }
+
+    .QrCode img{
+        width: 150px;
+        height: 150px;
+    }
+
+    button{
+        height: 30px;
+    }
+
+    .Adicionar{
+        width: 45%;
+        height: 40px;
+        background-color: white;
+        border: none;
+        border-radius: 15px;
+        box-shadow: 1px 1px 8px #ccc;
+    }
+`;
+
+export const Footer = styled.footer`
+    width: 100%;
+    background-color: #F7F8F9;
+    padding: 0px 30px;
+    margin: 20px 0px;
+
+    div{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+
+    p{
+        font-size: 16px;
+        font-weight: 500;
+    }
+
+    button{
+        width: 100%;
+    }
+    
 `;
