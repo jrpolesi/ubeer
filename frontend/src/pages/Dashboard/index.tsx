7051d4da3a3742505819f376f04021ea5a9b8e0b
Your Name <you@example.com>
2022-03-17 01:29:00 -0400

    feat: implementacao das rotas do mapa  feitas.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -11,39 +11,127 @@ import {
   List,
   Button,
 } from "grommet";
-import { Search, Menu } from "grommet-icons";
-import React, { useState } from "react";
+import { Search, Menu, LocationPin } from "grommet-icons";
+import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { TravelContext } from "../../providers/travel/index";
 import MapUbeer from "../../components/GoogleMaps";
+import axios from "axios";
 
 function DashBoard() {
-  const { travelStatus } = useContext(TravelContext);
-  const [title, newTtitle] = useState<string>("Localização Atual");
+  const { travelStatus, updateTravelStatus } = useContext(TravelContext);
 
   return (
     <>
-      <Header background="light-1" pad="small">
-        <Box background="white">
-          <Button
-            icon={<Menu color="accent-4" />}
-            hoverIndicator
-            onClick={() => console.log("oi")}
-          />
-        </Box>
-        {title}
-      </Header>
-      <MapUbeer />
-      <Card pad="small" background="accent-4" gap="medium">
-        <TextInput
-          placeholder="Digite o Local"
-          icon={<Search color="black" />}
-        />
-        <div>
-          <Heading>Favoritos</Heading>
-          <List primaryKey="local" data={[]} />
-        </div>
-      </Card>
+      {travelStatus === false ? (
+        <>
+          <Header background="light-1" pad="small">
+            <Box background="white">
+              <Button
+                icon={<Menu color="accent-4" />}
+                hoverIndicator
+                onClick={() => console.log("oi")}
+              />
+            </Box>
+            Localização Atual
+          </Header>
+          <MapUbeer />
+          <Card pad="small" background="accent-4" gap="medium">
+            <TextInput
+              placeholder="Digite o Local"
+              icon={<Search color="black" />}
+              onClick={() => updateTravelStatus("waiting for driver")}
+            />
+            <div>
+              <Heading>Favoritos</Heading>
+              <List primaryKey="local" data={[]} />
+            </div>
+          </Card>
+        </>
+      ) : travelStatus === "waiting for driver" ? (
+        <>
+          <Header background="light-1" pad="small">
+            <Box background="white">
+              <Button
+                icon={<Menu color="accent-4" />}
+                hoverIndicator
+                onClick={() => console.log("oi")}
+              />
+            </Box>
+            Selecione o Endereço
+          </Header>
+          <MapUbeer />
+          <Card pad="small" background="accent-4" gap="medium">
+            <TextInput
+              placeholder="Digite onde você está"
+              icon={<Search color="black" />}
+            />
+            <input />
+            <TextInput
+              placeholder="Digite para onde você quer ir"
+              icon={<LocationPin color="black" />}
+            />
+            <Button
+              color="black"
+              hoverIndicator
+              onClick={() => updateTravelStatus("in transit")}
+            >
+              Chamar Motorista
+            </Button>
+            <div>
+              <Heading>Favoritos</Heading>
+              <List
+                primaryKey="local"
+                data={[]}
+                //preciso passar um axios para consumir a API das rotas favoritas
+                onClick={() => console.log("oi")}
+              />
+            </div>
+          </Card>
+        </>
+      ) : travelStatus === "in transit" ? (
+        <>
+          <Header background="light-1" pad="small">
+            <Box background="white">
+              <Button
+                icon={<Menu color="accent-4" />}
+                hoverIndicator
+                onClick={() => console.log("oi")}
+              />
+            </Box>
+            Selecione o Endereço
+          </Header>
+          <MapUbeer />
+          <Card pad="small" background="accent-4" gap="medium">
+            <TextInput
+              placeholder="Digite onde você está"
+              icon={<Search color="black" />}
+            />
+            <TextInput
+              placeholder="Digite para onde você quer ir"
+              icon={<LocationPin color="black" />}
+            />
+            <Button
+              icon={<Menu color="accent-4" />}
+              hoverIndicator
+              onClick={() => updateTravelStatus("in transit")}
+            >
+              Chamar Motorista
+            </Button>
+            <div>
+              <Heading>Favoritos</Heading>
+              <List
+                primaryKey="local"
+                data={[]}
+                //preciso passar um axios para consumir a API das rotas favoritas
+                onClick={() => console.log("oi")}
+              />
+            </div>
+          </Card>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
