db3de4934cd356f41e4b2a22c119bf2ab210bbd5
Jose Ricardo Kremer Polesi <josericardo.kremer@gmail.com>
2022-03-19 02:48:31 -0300

    refactor: Altera estilos não usados no componente Modal

--- frontend/src/components/Modal/style.ts
+++ frontend/src/components/Modal/style.ts
@@ -3,12 +3,13 @@ import styled from "styled-components";
 export const Popup = styled.div`
   width: 99%;
   position: absolute;
-  top: 8;
+  bottom: 0;
   left: 0;
   z-index: 1;
   display: flex;
   flex-direction: column;
   color: #97adb6;
+  background-color: white;
   border-radius: 16px 16px 0px 0px;
 
   section {
@@ -20,40 +21,6 @@ export const Popup = styled.div`
   h2 {
     margin-top: 15px;
     font-weight: bold;
-  }
-`;
-
-export const PopupWaiting = styled.div`
-  width: 99%;
-  height: 483px;
-  position: absolute;
-  top: 8;
-  left: 0;
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  color: #97adb6;
-  border-radius: 16px 16px 0px 0px;
-
-  section {
-    margin-top: 10px;
-    width: 90%;
-  }
-
-  button {
-    background-color: #fbd50e;
-    width: 100%;
-    height: 48px;
-    color: white;
-    border-radius: 15px;
-    margin-top: 15px;
-    border: solid 2px #fbd50e;
-    cursor: pointer;
-    font-size: 18px;
-    font-weight: 700;
-    line-height: 28px;
-    text-align: center;
   }
 `;
 
