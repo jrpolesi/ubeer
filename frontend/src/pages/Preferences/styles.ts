08241a153718f639580691e92b65a5bc15afdeb0
Robert <lober3464@gmail.com>
2022-03-21 19:28:19 -0300

    feat: Adicionando estilização das fontes para a página de preferências

--- frontend/src/pages/Preferences/styles.ts
+++ frontend/src/pages/Preferences/styles.ts
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 export const Main = styled.main`
   height: 100%;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 18px;
+  color: #4b545a;
 `;
 
 export const Section = styled.section`
@@ -9,7 +15,7 @@ export const Section = styled.section`
   justify-content: space-evenly;
   margin: 20px 0px;
 
-  div{
+  div {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -19,7 +25,7 @@ export const Section = styled.section`
     box-shadow: 1px 1px 8px #ccc;
     border-radius: 5px;
   }
-  img{
+  img {
     margin-bottom: 10px;
   }
 `;
@@ -30,19 +36,18 @@ export const Footer = styled.footer`
   justify-content: center;
   margin-top: 40px;
 
-  figure{
+  figure {
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #FBD50E;
-    border-radius: 90px; 
+    background-color: #fbd50e;
+    border-radius: 90px;
     box-shadow: 1px 1px 8px #ccc;
     width: 65px;
     height: 65px;
     cursor: pointer;
   }
 
-  img{
-
+  img {
   }
 `;
