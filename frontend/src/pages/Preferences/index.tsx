499ae16b1ebbb567b3b92b25cc6a41068e2e87d1
Your Name <you@example.com>
2022-03-22 11:10:46 -0400

    feat: icones corrigidos

--- frontend/src/pages/Preferences/index.tsx
+++ frontend/src/pages/Preferences/index.tsx
@@ -10,13 +10,20 @@ import Favoritos from "../../assets/img/iconFavoritos.png";
 import Suporte from "../../assets/img/iconSuporte.png";
 import Cancel from "../../assets/img/iconCancel.png";
 import bebado from "../../assets/img/bebado.jpg";
-import { Car, MailOption, User } from "grommet-icons";
+import {
+  Car,
+  Favorite,
+  History,
+  MailOption,
+  Support,
+  User,
+  UserSettings,
+} from "grommet-icons";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { UserContext } from "../../providers/user";
 
 const Preferences = () => {
-
   const schema = yup.object().shape({
     name: yup.string().required("Campo obrigatório"),
     email: yup.string().email("Email inválido").required("Campo obrigatório"),
@@ -34,10 +41,14 @@ const Preferences = () => {
       .matches(/^[a-zA-Z]{3}-[0-9]{4}$/, "Placa inválida"),
   });
 
-  const {register, handleSubmit, formState: { errors }} = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
-  
+
   const editUser = ({ model, plate, email, password, name }: FieldValues) => {
     const userFormated = {
       email,
@@ -46,17 +57,15 @@ const Preferences = () => {
       car: { model, plate },
     };
   };
-  
+
   const [bill, setBill] = useState(false);
   const [favorite, setFavorite] = useState(false);
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
-  return(
+  return (
     <>
-      <Header title="Preferências"/>
-
-      
+      <Header title="Preferências" />
 
       <Main>
         {bill && (
@@ -116,42 +125,81 @@ const Preferences = () => {
               <button onClick={() => setFavorite(false)}>X</button>
               <h2>Favoritos</h2>
             </div>
-            {user?.favoritesPlaces.length === 0 ? 
-              (
-                <aside>
-                  <p>Sem favoritos salvos</p>
-                  <figure>
-                    <img src={bebado} alt="Imagem de um bebado" />
-                  </figure>
-                </aside>
-              ):
-              (
-                <ul>
-                  {user?.favoritesPlaces.map((favorite) => 
-                    <li key={favorite.id}>
-                      <p>{favorite.name}</p>
-                    </li>)}
-                </ul>
-              )}
+            {user?.favoritesPlaces.length === 0 ? (
+              <aside>
+                <p>Sem favoritos salvos</p>
+                <figure>
+                  <img src={bebado} alt="Imagem de um bebado" />
+                </figure>
+              </aside>
+            ) : (
+              <ul>
+                {user?.favoritesPlaces.map((favorite) => (
+                  <li key={favorite.id}>
+                    <p>{favorite.name}</p>
+                  </li>
+                ))}
+              </ul>
+            )}
           </SectionContainer>
         )}
         <Section>
           <div onClick={() => setBill(true)}>
-            <img src={Conta} alt="Conta" />
+            <div
+              style={{
+                width: "82px",
+                height: "82px",
+                background: "#FBD50E",
+                borderRadius: "50%",
+              }}
+            >
+              <UserSettings
+                color="white"
+                size="42px"
+                style={{ padding: "14px" }}
+              />
+            </div>
             <p>Conta</p>
           </div>
           <div onClick={() => setFavorite(true)}>
-            <img src={Favoritos} alt="Favoritos" />
+            <div
+              style={{
+                width: "82px",
+                height: "82px",
+                background: "#FBD50E",
+                borderRadius: "50%",
+              }}
+            >
+              <Favorite color="white" size="42px" style={{ padding: "14px" }} />
+            </div>
             <p>Favoritos</p>
           </div>
         </Section>
         <Section>
           <div onClick={() => navigate("/history")}>
-            <img src={Conta} alt="Histórico" />
+            <div
+              style={{
+                width: "82px",
+                height: "82px",
+                background: "#FBD50E",
+                borderRadius: "50%",
+              }}
+            >
+              <History color="white" size="42px" style={{ padding: "14px" }} />
+            </div>
             <p>Histórico</p>
           </div>
           <div onClick={() => navigate("/support")}>
-            <img src={Suporte} alt="Suporte" />
+            <div
+              style={{
+                width: "82px",
+                height: "82px",
+                background: "#FBD50E",
+                borderRadius: "50%",
+              }}
+            >
+              <Support color="white" size="42px" style={{ padding: "14px" }} />
+            </div>
             <p>Suporte</p>
           </div>
         </Section>
@@ -161,7 +209,6 @@ const Preferences = () => {
           <img src={Cancel} alt="Botão para voltar" />
         </figure>
       </Footer>
-
     </>
   );
 };
