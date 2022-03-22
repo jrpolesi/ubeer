9557672732a15f7ebdff11166ed04911cf0edab2
Your Name <you@example.com>
2022-03-21 20:43:27 -0400

    feat: estilzacao alterada do componente CardTRavel

--- frontend/src/components/ModalFinishedTravel/styles.ts
+++ frontend/src/components/ModalFinishedTravel/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   position: absolute;
-  min-width: 400px;
+  min-width: 315px;
   transform: translate(-50%, -50%);
   left: 50%;
   top: 50%;
