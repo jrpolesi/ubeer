3827df5ee8bacdcd357f7630e76e8caf67d5c549
Your Name <you@example.com>
2022-03-23 11:08:34 -0400

    feat: pesquisa de endereco atualizado

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,11 +2,11 @@ import React, { ButtonHTMLAttributes } from "react";
 import { Container } from "./styles";
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: string;
-  bg?: string
+  bg?: string;
 }
 
-const Button = ({ children, onClick, variant, bg }: Props) => (
-  <Container bg={bg} onClick={onClick} variant={variant}>
+const Button = ({ children, onClick, variant, bg, ...rest }: Props) => (
+  <Container bg={bg} onClick={onClick} variant={variant} {...rest}>
     {children}
   </Container>
 );
